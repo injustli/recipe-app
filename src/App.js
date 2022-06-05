9f29b169a3a48ddd9825921e487e40f70e14bbbd
Lord-Bubbles <justinhanyueli@gmail.com>
2022-06-05 17:36:09 -0500

    Added a test API method to see if server is actually working

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://recipe-app-351220.uc.r.appspot.com/api", {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => this.setState({data: data.message}));
+  }
 
   render() {
     return (
       <div className="App">
-        <p>
-          Hello World!
-        </p>
+        <p>{!this.state.data ? "Loading..." : this.state.data}</p>
       </div>
     );
   }
