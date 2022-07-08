4888e2da84b8c8aa08a3b1029c47ccccaa9847c3
Lord-Bubbles <justinhanyueli@gmail.com>
2022-07-08 12:46:14 -0500

    moved the modal to a new component for ease of reuse

--- src/components/SearchAndFilter.js
+++ src/components/SearchAndFilter.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {Container, InputGroup, Modal, Button, Form, FormControl} from "react-bootstrap";
+import {Container, InputGroup, Button, FormControl} from "react-bootstrap";
 import {BsSearch} from "react-icons/bs";
-import "../styles/SearchAndFilter.css";
+import ModalForm from "./ModalForm";
 
 class SearchAndFilter extends React.Component {
 
@@ -14,7 +14,6 @@ class SearchAndFilter extends React.Component {
       minTime: 0,
       maxTime: 0,
       createdBy: "",
-      inputFields: []
     };
   }
 
@@ -23,67 +22,24 @@ class SearchAndFilter extends React.Component {
     this.setState({modalOpen: flag});
   }
 
-  // Keeps track of the user input for recipe name
-  setName = (event) => {
-    this.setState({name: event.target.value})
+  // Call back function to set name
+  setName = (name) => {
+    this.setState({name: name})
   }
 
-  // Handles getting the ingredients the user inputted to filter on
-  setIngredient = (index, event) => {
-    const ingredients = [...this.state.ingredients];
-    if (!ingredients[index]) {
-      ingredients.push(event.target.value);
-    } else {
-      ingredients[index] = event.target.value;
-    }
+  // Call back function to set min and max time
+  setTime = (min, max) => {
+    this.setState({minTime: min, maxTime: max});
+  }
+
+  // Call back function to set list of ingredients
+  setIngredients = (ingredients) => {
     this.setState({ingredients: ingredients});
   }
 
-  // Handles creating a new input field with an option to add more every time the user clicks the plus button
-  addInput = () => {
-    const {inputFields, ingredients} = this.state;
-    const inputs = [...inputFields];
-    const length = inputFields.length;
-    inputs.push(
-      <React.Fragment key={length}>
-        <label>Ingredient:
-          <input type="text" value={ingredients[length]} onChange={(event) => this.setIngredient(length, event)}/>
-        </label>
-        <button id={`form-add-${length}`} onClick={this.addInput} type="button">+</button>
-        <br></br>
-      </React.Fragment>
-    )
-    this.setState({inputFields: inputs}, () => {
-      if (length != 0) {
-        document.getElementById(`form-add-${length-1}`).classList.add("hidden");
-      }
-    });
-    document.getElementById("form-add").classList.add("hidden");
-  }
-
-  // Keeps track of the user input for min time
-  setMin = (event) => {
-    this.setState({minTime: event.target.value});
-  }
-
-  // Keeps track of the user input for max time
-  setMax = (event) => {
-    this.setState({maxTime: event.target.value});
-  }
-
-  // Keeps track of the user input for created by
-  setCreator = (event) => {
-    this.setState({createdBy: event.target.value});
-  }
-
-  // Returns a valid query parameter string depending on the ingredients state
-  createIngredientQueryParam = ()  => {
-    let res = "";
-    let {ingredients} = this.state;
-    for (let i in ingredients) {
-      res += `ingredients=${ingredients[i]}&`
-    }
-    return res;
+  // Call back function to set creator
+  setCreator = (user) => {
+    this.setState({createdBy: user});
   }
 
   // Filters what gets displayed based on the user inputs
@@ -101,28 +57,39 @@ class SearchAndFilter extends React.Component {
     .then(data => this.props.setRecipes(data));
   }
 
+  // Returns a valid query parameter string depending on the ingredients state
+  createIngredientQueryParam = ()  => {
+    let res = "";
+    let {ingredients} = this.state;
+    for (let i in ingredients) {
+      res += `ingredients=${ingredients[i]}&`
+    }
+    return res;
+  }
+
   // Used by the search bar to filter the recipes displayed
   onSearch = async (event) => {
-    this.setState({name: event.target.value}, await this.filterRecipes());
-  }
-
-  // Used by the submit button to filter the recipes displayed and closes modal window
-  onSubmit = async () => {
+    this.setName(event.target.value);
     await this.filterRecipes();
-    this.reset();
   }
 
-  // Clears the form and closes the modal window
-  reset = () => {
-    this.setState({
-      modalOpen: false,
-      name: "",
-      ingredients: [],
-      minTime: 0,
-      maxTime: 0,
-      createdBy: "",
-      inputFields: []
-    });
+  // Render the modal form if modal is open; otherwise render nothing
+  renderForm = () => {
+    if (this.state.modalOpen) {
+      return (
+      <ModalForm
+        setModal={flag => this.setModal(flag)}
+        setName={name => this.setName(name)}
+        name={this.state.name}
+        modalOpen={this.state.modalOpen}
+        setTime={(min, max) => this.setTime(min, max)}
+        setIngredients={(ingredients) => this.setIngredients(ingredients)}
+        setCreator={(user) => this.setCreator(user)}
+        filterRecipes={() => this.filterRecipes()}
+      />
+      );
+    }
+    return null;
   }
 
   render() {
@@ -130,52 +97,18 @@ class SearchAndFilter extends React.Component {
       <Container>
         <InputGroup>
           <FormControl 
+            as="input"
             type="text"
             placeholder="Search"
-            aria-label="Search"
             onChange={this.onSearch}
+            value={this.state.name}
           />
           <InputGroup.Text>
             <BsSearch />
           </InputGroup.Text>
         </InputGroup>
         <Button onClick={() => this.setModal(true)} type="button">Advanced Search</Button>
-        <Modal 
-          show={this.state.modalOpen} 
-          onHide={this.reset} 
-          centered="true"
-          size="lg"
-        >
-          <Modal.Header closeButton/>
-          <Modal.Body className="formBody">
-            <Form>
-              <label>Name: 
-                <input type="text" value={this.state.name} onChange={this.setName} />
-              </label>
-              <br></br>
-              <label>Ingredients: </label>
-              <button id="form-add" onClick={this.addInput} type="button">+</button>
-              <br></br>
-              {this.state.inputFields}
-              <br></br>
-              <label>Min Time:
-                <input type="number" value={this.state.minTime} onChange={this.setMin} />
-              </label>
-              <br></br>
-              <label>Max Time:
-                <input type="number" value={this.state.maxTime} onChange={this.setMax} />
-              </label>
-              <br></br>
-              <label>Created By:
-                <input type="text" value={this.state.createdBy} onChange={this.setCreator} />
-              </label>
-              <br></br>
-              <div style={{textAlign: "center"}}>
-                <Button onClick={this.onSubmit} type="submit">Submit</Button>
-              </div>
-            </Form>
-          </Modal.Body>
-        </Modal>
+        {this.renderForm()}
       </Container>
     );
   }
