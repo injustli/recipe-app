fb69792e43c273e0979ebc23052ee9d8019a7258
Lord-Bubbles <justinhanyueli@gmail.com>
2023-06-21 18:25:46 -0700

    fixed necessary changes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,12 +2,13 @@ const asyncHandler = require("express-async-handler");
 const User = require("../database/UserModel");
 
 // @desc   Adds new user if it doesnt exist and returns newly created user, otherwise return existing user from database
+//         A unique user is defined by its email since every Google Account must have a unique email address.
 // @route  PUT /users
 // @access Public
 const addOrUpdateUser = asyncHandler(async (req, res) => {
   const { email, name } = req.body;
-  // Finds a user based off email and name and returns an object with only the id, email, name, recipes fields
-  let user = await User.findOne({ email: email, name: name }, "_id email name recipes").exec();
+  // Finds a user based off email and name and returns an object with only the id, email, name fields
+  let user = await User.findOne({ email: email }, "_id email name").exec();
   if (user) {
     res.status(200).json({ 
       data: user, 
@@ -21,7 +22,6 @@ const addOrUpdateUser = asyncHandler(async (req, res) => {
           _id: user._id,
           email: user.email,
           name: user.name,
-          recipes: user.recipes,
         },
         message: "New user created successfully!"
       });
