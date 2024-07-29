74697de9e23a73da74c50e831b9457261fdd0f0e
Lord-Bubbles <justinhanyueli@gmail.com>
2024-07-28 22:09:09 -0500

    Move the connectDB function within server start statement

--- server/server.js
+++ server/server.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(path.resolve(), '/build')));
 });
 }
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log(`Listening on port ${process.env.PORT || 8080}!`)
-);
+app.listen(process.env.PORT || 8080, async () => {
+  await connectDB();
+  console.log(`Listening on port ${process.env.PORT || 8080}!`);
+});
