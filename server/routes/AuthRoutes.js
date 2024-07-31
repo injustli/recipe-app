c8bc8d7e74ade801d8ddfcbe954bb39dc02fd951
Lord-Bubbles <justinhanyueli@gmail.com>
2024-07-31 00:50:08 -0500

    Create auth routes for login, logout, refresh access token and add middleware to logout route

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,10 +1,22 @@
 import express from 'express';
-import { fetchTokens } from '../controllers/AuthController.js';
+import { refresh, userLogin, userLogout } from '#controllers/AuthController';
+import { validateMiddleware } from '#authorization/Auth';
 
 const router = express.Router();
+
 // @desc   Returns id_token, refresh_token, access_tokens
-// @route  POST /api/auth/google
+// @route  POST /api/auth/google/authenticate
 // @access public
-router.post('/', fetchTokens);
+router.post('/google/authenticate', userLogin);
+
+// @desc   Returns id_token, refresh_token, access_tokens
+// @route  POST /api/auth/google/logout
+// @access public
+router.post('/google/logout', validateMiddleware, userLogout);
+
+// @desc   Returns id_token, refresh_token, access_tokens
+// @route  POST /api/auth/google/logout
+// @access public
+router.post('/google/refresh', refresh);
 
 export default router;
