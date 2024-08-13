9b7f4e88c28b328ae3fb2ae5515d768f75cb8b77
Lord-Bubbles <justinhanyueli@gmail.com>
2024-08-12 22:40:02 -0500

    Replace bootstrap components with mantine components

--- client/src/components/SearchModalForm.jsx
+++ client/src/components/SearchModalForm.jsx
@@ -1,5 +1,5 @@
-import { useState, useRef } from 'react';
-import { Modal, Button, Form, Row, Col, InputGroup } from 'react-bootstrap';
+import { useRef, useState } from 'react';
+import { Modal, Button, Box, TextInput, Group, RangeSlider, Flex } from '@mantine/core';
 
 // Renders the advanced search form
 export default function SearchModalForm(props) {
@@ -41,105 +41,83 @@ export default function SearchModalForm(props) {
   };
 
   return (
-    <Modal show={modalOpen} onHide={() => setModal(false)} centered size="lg">
-      <Modal.Header closeButton />
-      <Modal.Body>
-        <Form id="search-form" noValidate validated={validated}>
-          <Form.Group className="mb-3">
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Recipe Name"
-              value={recipeName}
-              onChange={(e) => setRecipeName(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Ingredients</Form.Label>
-            <InputGroup className="mb-3">
-              <Form.Control
-                type="text"
-                placeholder="Ingredient Name"
-                ref={inputRef}
-              />
-              <Button
-                onClick={(e) => {
-                  updateIngredients((currentArray) => {
-                    return [...currentArray, inputRef.current.value];
-                  });
-                }}
-                type="button"
-                variant="outline-dark"
-              >
-                Add
-              </Button>
-            </InputGroup>
-            <ul>
-              {ingredients.map((ingredient, index) => (
-                <li key={ingredient.id}>
-                  {ingredient}
-                  <Button
-                    onClick={() => {
-                      updateIngredients((currentArray) => {
-                        return currentArray.filter((a, idx) => idx !== index);
-                      });
-                    }}
-                    type="button"
-                    variant="outline-dark"
-                    style={{ marginLeft: '5px' }}
-                  >
-                    Delete
-                  </Button>
-                </li>
-              ))}
-            </ul>
-          </Form.Group>
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="validationCustom01">
-              <Form.Label>Mininum Cooking Time</Form.Label>
-              <Form.Control
-                type="number"
-                value={minTime}
-                onChange={(e) => updateMinTime(e.target.value)}
-                min="0"
-                max={maxTime}
-                placeholder="0"
-              />
-              <Form.Control.Feedback type="invalid">
-                Minimum cooking time must be a value between 0 and maximum
-                cooking time!
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} controlId="validationCustom02">
-              <Form.Label>Maximum Cooking Time</Form.Label>
-              <Form.Control
-                type="number"
-                value={maxTime}
-                onChange={(e) => updateMaxTime(e.target.value)}
-                min={minTime}
-                placeholder="0"
-              />
-              <Form.Control.Feedback type="invalid">
-                Maximum cooking time must be at least minimum cooking time!
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Form.Group className="mb-3">
-            <Form.Label>Created By</Form.Label>
-            <Form.Control
-              type="text"
-              value={creator}
-              onChange={(e) => updateCreator(e.target.value)}
-              placeholder="Username"
-            />
-          </Form.Group>
-          <div className="d-flex justify-content-center">
-            <Button type="button" onClick={onSubmit}>
-              Submit
+    <Modal
+      opened={modalOpen}
+      onClose={() => setModal(false)}
+      centered
+      size="lg"
+      closeOnClickOutside={false}
+      closeOnEscape={false}
+    >
+      <form noValidate onSubmit={onSubmit}>
+        <TextInput
+          label="Name"
+          placeholder="Recipe Name"
+          value={recipeName}
+          onChange={(e) => setRecipeName(e.target.value)}
+          mb="sm"
+        />
+        <Box>
+          <Group mb="sm">
+            <TextInput label="Ingredients" placeholder="Ingredient Name" ref={inputRef} />
+            <Button
+              onClick={() => {
+                updateIngredients((currentArray) => {
+                  return [...currentArray, inputRef.current.value];
+                });
+              }}
+              variant="outline"
+            >
+              Add
             </Button>
-          </div>
-        </Form>
-      </Modal.Body>
+          </Group>
+          <ul>
+            {ingredients.map((ingredient, index) => (
+              <li key={ingredient.id}>
+                {ingredient}
+                <Button
+                  onClick={() => {
+                    updateIngredients((currentArray) => {
+                      return currentArray.filter((_, idx) => idx !== index);
+                    });
+                  }}
+                  variant="outline"
+                  ml={5}
+                >
+                  Delete
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </Box>
+        <RangeSlider
+          defaultValue={[time.min, time.max]}
+          step={1}
+          min={0}
+          max={360}
+          minRange={1}
+          onChange={(val) => {
+            updateMinTime(val[0]);
+            updateMaxTime(val[1]);
+          }}
+          value={[minTime, maxTime]}
+        />
+        
+        <Form.Group className="mb-3">
+          <Form.Label>Created By</Form.Label>
+          <Form.Control
+            type="text"
+            value={creator}
+            onChange={(e) => updateCreator(e.target.value)}
+            placeholder="Username"
+          />
+        </Form.Group>
+        <Flex justify="center">
+          <Button variant="filled" type="submit">
+            Search
+          </Button>
+        </Flex>
+      </form>
     </Modal>
   );
 }
