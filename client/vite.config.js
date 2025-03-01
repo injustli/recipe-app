9e9036c2e5a975ee2ee972ad1e213a8dac5c92e9
Justin Li <justinhanyueli@gmail.com>
2025-03-01 01:30:15 -0600

    Update frontend deployment (#72)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') }
-  },
-  server: {
-    port: 3000,
-    host: true,
-    watch: {
-      usePolling: true
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_NODE_ENV == 'production' ? '/recipe-app' : '/',
+    plugins: [react()],
+    resolve: {
+      alias: { '@': path.resolve(__dirname, './src') }
+    },
+    server: {
+      port: 3000,
+      host: true,
+      watch: {
+        usePolling: true
+      }
     }
-  }
+  };
 });
