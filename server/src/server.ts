d38e659ddff56e05a7e98191ba7bbb2ab5d95637
injustli <justinhanyueli@gmail.com>
2025-08-30 22:52:04 -0500

    Fix test endpoint error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import bodyParser from 'body-parser';
-import express, { Response } from 'express';
+import express, { Response, Request } from 'express';
 import connectDB from '@database/Config';
 import recipeRoutes from '@routes/RecipeRoutes';
 import authRoutes from '@routes/AuthRoutes';
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // Test API method
-app.get('/api/test', (res: Response) => {
+app.get('/api/test', (_req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello World!' });
 });
 
